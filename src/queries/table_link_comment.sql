4da5361be74555d552ad169a7835a413583ba2c9
Manitary <pak.ska@gmail.com>
2023-04-21 20:56:06 +0100

    Add imgur link pairing table

--- src/queries/table_link_comment.sql
+++ src/queries/table_link_comment.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS link (
+CREATE TABLE IF NOT EXISTS comment_link (
     id TEXT NOT NULL PRIMARY KEY UNIQUE
     , new_id TEXT UNIQUE
     , FOREIGN KEY(id) REFERENCES tree(comment_id)
