efbc70672cb71d131ebe4997fc0568a16247ebb2
Manitary <pak.ska@gmail.com>
2023-04-23 01:31:54 +0100

    Update table schema
    Update rewatch columns to match readily available info.
    
    Add uniqueness constraint and foreign keys constraints.

--- src/queries/table_rewatch_episodes.sql
+++ src/queries/table_rewatch_episodes.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS episode (
     id INTEGER NOT NULL
     , post_id TEXT NOT NULL
     , title TEXT
-    , FOREIGN KEY(id) REFERENCES rewatch(id)
     , PRIMARY KEY(id, post_id)
+    , FOREIGN KEY(id) REFERENCES rewatch(id) ON DELETE RESTRICT ON UPDATE CASCADE
 )
--- src/queries/table_rewatch_list.sql
+++ src/queries/table_rewatch_list.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS rewatch (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
-    , rewatch_name TEXT NOT NULL
-    , sub_name TEXT
-    , date_start TEXT
-    , date_end TEXT
-    , host TEXT
+    , rewatch_name TEXT NOT NULL -- name of the rewatch series
+    , alt_name TEXT -- usually reserved to eng/jp alternate name
+    , sub_name TEXT -- optional for further division (e.g. seasons)
+    , rewatch_year INT
+    , hosts TEXT
     , processed INTEGER NOT NULL DEFAULT 0
+    , UNIQUE(rewatch_name, alt_name, sub_name, hosts, rewatch_year)
 )
