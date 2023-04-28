baa97e3b29b588611adccca537f182294f671be1
Manitary <pak.ska@gmail.com>
2023-04-28 19:26:47 +0100

    fix foreign key in discussion comment tree table

--- src/queries/discussion/table_setup.sql
+++ src/queries/discussion/table_setup.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS comment_tree (
     comment_id TEXT NOT NULL PRIMARY KEY UNIQUE
     , post_id TEXT NOT NULL
     , parent_id TEXT -- None for top level comments
-    , FOREIGN KEY(post_id) REFERENCES discussion(post_id) ON DELETE RESTRICT ON UPDATE CASCADE
+    , FOREIGN KEY(post_id) REFERENCES episode(post_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS imgur_link (
